5c6a602c535b2192137a8a7e86964fdbf047e742
jianghs <jianghs_own@163.com>
2018-05-20 00:28:52 +0800

    chapter2 commit

---
+++ src/main/java/org/smart4j/controller/CustomerServlet.java
@@ -0,0 +1,32 @@
+package org.smart4j.controller;
+
+import org.smart4j.model.Customer;
+import org.smart4j.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        request.setAttribute("customerList", customerList);
+        request.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(request, response);
+    }
+}
---
+++ src/test/resources/sql/customer_init.sql
@@ -0,0 +1,3 @@
+TRUNCATE customer;
+INSERT INTO customer (name, contact, telephone, email, remark) VALUE ('customer1', 'Jack', '123423123', 'jack@123.com', NULL );
+INSERT INTO customer (name, contact, telephone, email, remark) VALUE ('customer2', 'Rose', '1231237788', 'rose@123.com', NULL );
